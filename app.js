5f0a2fb7133f9ba152c6f1a489ced9aefeb90d85
root <root@ASCPC.ASCPC.local>
2025-07-18 13:48:17 -0400

    dependency updates

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 80
+const Example = require('@wkl-training/ascpc-npm-example');
+
+const example = new Example();
+
+process.env.SECRET_KEY = '1234321';
+process.env.DB_USER = 'words';
+process.env.DB_PASSWORD = 'morewords';
 
 app.get('/', (req, res) => {
-  res.send('Welcome to your ASCPC web app!')
+  res.send(example.sayHello())
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
